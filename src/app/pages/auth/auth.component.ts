3c05faf7605165e731b558e52938ef02cb9f6ce9
Luis henrique <105991145+LuissHL@users.noreply.github.com>
2024-03-04 18:07:24 -0300

    created structure

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatTabsModule, SignInComponent, SignUpComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
